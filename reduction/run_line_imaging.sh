c99ea254dc2be72782d0c301e76962010a24b3be
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-11-17 18:12:16 -0700

    attempted to do a lot of new imaging parallel work...

--- reduction/run_line_imaging.sh
+++ reduction/run_line_imaging.sh
@@ -4,10 +4,18 @@
 #in the configure file to submit.  That submit script will create the
 #clusterspec file for us in the WORK_DIR we specified in the configure file.
 
-WORK_DIR='/lustre/aginsbur/sgrb2/2016.1.00550.S/reduction'
+WORK_DIR='/lustre/aginsbur/sgrb2/2016.1.00550.S/calibrated'
+WORK_DIR='/lustre/aginsbur/sgrb2/2016.1.00550.S/science_goal.uid___A001_X879_X160/group.uid___A001_X879_X161/member.uid___A001_X879_X162/calibrated'
 cd ${WORK_DIR}
+REDUCTION_DIR='/lustre/aginsbur/sgrb2/2016.1.00550.S/reduction'
 
-# casa's python requires a DISPLAY for matplot so create a virtual X server
-xvfb-run -d casa-prerelease --nogui --nologger -c "execfile('$WORK_DIR/scriptForImaging_lines.py')"
-#xvfb-run casapy --nogui -c "spwlist='0'; $SCRIPTPATH/fullcube_r0.py"
-#xvfb-run casa --nogui -c "spwlist='0'; execfile('$SCRIPTPATH/fullcube_r0.py')"
+## casa's python requires a DISPLAY for matplot so create a virtual X server
+#xvfb-run -d casa --nogui --nologger -c "execfile('$REDUCTION_DIR/scriptForImaging_lines.py')"
+
+export CASAPATH=/home/casa/packages/RHEL6/release/casa-release-5.1.0-74
+export PATH=${CASAPATH}/bin:$PATH
+
+export CASACMD="execfile('$REDUCTION_DIR/scriptForImaging_lines.py')"
+echo $CASACMD
+
+mpicasa -machinefile $PBS_NODEFILE casa -quiet --nogui --nologger --log2term -c "${CASACMD}"
--- reduction/scriptForImaging_lines.py
+++ reduction/scriptForImaging_lines.py
@@ -1,67 +1,88 @@
 
+import numpy as np
 import os
 import glob
+import datetime
 
-def makefits(myimagebase):
+def makefits(myimagebase, cleanup=True):
     impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True) # perform PBcorr
     exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', dropdeg=True, overwrite=True) # export the corrected image
     exportfits(imagename=myimagebase+'.pb', fitsimage=myimagebase+'.pb.fits', dropdeg=True, overwrite=True) # export the PB image
     exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', dropdeg=True, overwrite=True) # export the PB image
     exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
 
+    if cleanup:
+        for suffix in ('psf', 'weight', 'sumwt', 'pb', 'model', 'residual',
+                       'mask', 'image'):
+            os.system('rm -rf {0}.{1}'.format(myimagebase, suffix))
+
+
+if 'spwlist' not in locals():
+    spwlist = ((0,25),(1,27),(2,29),(3,31))[::-1]
+    print("Setting spwlist to {0}".format(spwlist))
+
 
 
 mslist = ['uid___A002_Xc44eb5_X1139.ms.split.cal', 'uid___A002_Xc483da_Xa88.ms.split.cal']
 
 for ms in mslist:
-    listobs(ms, listfile=ms+'.listobs')
+    listobs(ms, listfile=ms+'.listobs', overwrite=True)
 
-for spw,spw_orig in enumerate((25,27,29,31)):
+for spw,spw_orig in spwlist:
 
-    for suffix, niter in (('dirty', 0), ('clarkclean1000', 1000), ):
+    for suffix, niter in (('clarkclean1000', 1000), ):
+        
+        step = 1920/32
+        for startchan in np.arange(0, 1920, step):
 
-        imagename = 'sgr_b2m.M.spw{0}.lines.{1}'.format(spw, suffix)
-        tclean(vis=mslist,
-               imagename=imagename,
-               datacolumn='data',
-               spw=['{0}'.format(spw), '{0}'.format(spw)],
-               field=['4', '5'],
-               specmode='cube',
-               start='',
-               outframe='LSRK',
-               nchan=-1,
-               threshold='10mJy',
-               imsize=[6000, 6000],
-               cell=['0.007arcsec'],
-               niter=niter,
-               deconvolver='clark',
-               gridder='standard',
-               weighting='briggs',
-               robust=0.5,
-               pbcor=True,
-               pblimit=0.2,
-               interactive=False)
-        makefits(imagename)
+            imagename = 'sgr_b2m.N.spw{0}.lines{2}-{3}.{1}'.format(spw, suffix, startchan, startchan+step)
+            if not os.path.exists("{0}.image.pbcor.fits".format(imagename)):
+                print("Imaging {0} at {1}".format(imagename, datetime.datetime.now()))
+                tclean(vis=mslist,
+                       imagename=imagename,
+                       datacolumn='data',
+                       spw=['{0}'.format(spw), '{0}'.format(spw)],
+                       field=['5', '6'],
+                       specmode='cube',
+                       outframe='LSRK',
+                       start=startchan,
+                       nchan=step,
+                       threshold='1mJy',
+                       imsize=[6000, 6000],
+                       cell=['0.007arcsec'],
+                       niter=niter,
+                       deconvolver='clark',
+                       gridder='standard',
+                       weighting='briggs',
+                       robust=0.5,
+                       pbcor=True,
+                       pblimit=0.2,
+                       savemodel='none',
+                       interactive=False)
+                makefits(imagename)
 
-        imagename = 'sgr_b2m.N.spw{0}.lines.{1}'.format(spw, suffix)
-        tclean(vis=mslist,
-               imagename=imagename,
-               datacolumn='data',
-               spw=['{0}'.format(spw), '{0}'.format(spw)],
-               field=['5', '6'],
-               specmode='cube',
-               start='',
-               outframe='LSRK',
-               nchan=-1,
-               threshold='1mJy',
-               imsize=[6000, 6000],
-               cell=['0.007arcsec'],
-               niter=niter,
-               deconvolver='clark',
-               gridder='standard',
-               weighting='briggs',
-               robust=0.5,
-               pbcor=True,
-               pblimit=0.2,
-               interactive=False)
-        makefits(imagename)
+            imagename = 'sgr_b2m.M.spw{0}.lines{2}-{3}.{1}'.format(spw, suffix, startchan, startchan+step)
+            if not os.path.exists("{0}.image.pbcor.fits".format(imagename)):
+                print("Imaging {0} at {1}".format(imagename, datetime.datetime.now()))
+                tclean(vis=mslist,
+                       imagename=imagename,
+                       datacolumn='data',
+                       spw=['{0}'.format(spw), '{0}'.format(spw)],
+                       field=['4', '5'],
+                       specmode='cube',
+                       start=startchan,
+                       nchan=step+2,
+                       outframe='LSRK',
+                       threshold='10mJy',
+                       imsize=[6000, 6000],
+                       cell=['0.007arcsec'],
+                       niter=niter,
+                       deconvolver='clark',
+                       gridder='standard',
+                       weighting='briggs',
+                       robust=0.5,
+                       pbcor=True,
+                       pblimit=0.2,
+                       savemodel='none',
+                       interactive=False)
+                makefits(imagename)
